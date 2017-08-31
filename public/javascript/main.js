6ab4cbf1f41cf7abf254f5329daf9e89009a6cc8
Alexandre Cetto <alexandrecetto@gmail.com>
2017-08-31 09:41:28 +0200

    feat(view): add a better display of the results

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -13,8 +13,13 @@
     httpRequest.onreadystatechange = alertContents;
     httpRequest.open('POST', '/search_fields_view');
 		httpRequest.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-		const parameters = getParameters();
-		console.log(parameters);
+		let parameters = getParameters();
+
+    if(parameters.words.length == 0 || parameters.fields.length == 0) {
+      document.getElementById('results').innerHTML = 'Invalid Request : missing arguments';
+      return;
+    }
+    parameters = writeRequestParameters(parameters.fields, parameters.words);
     httpRequest.send(parameters);
   }
 
@@ -35,17 +40,61 @@
   function getParameters() {
   	const words = document.getElementById('words').value;
   	const fields = document.getElementsByName('fields');
+
   	let checkedFields = [];
   	for (let i = 0; i < fields.length; i++) {
   		if (fields[i].checked) 
   			checkedFields.push(`${fields[i].value}`);
   	};
+  	return {
+      fields: checkedFields, 
+      words: words
+    }
   	
-  	return `fields=${checkedFields}&words=${words}`;
+  }
+
+  function writeRequestParameters(fields, words) {
+    return `fields=${fields}&words=${words}`;
   }
 
   function writeBackResults(results) {
   	const div = document.getElementById('results');
-  	div.innerHTML = (JSON.parse(results));
+
+    // remove possible old results.
+    div.innerHTML = '';
+
+    results = JSON.parse(results);
+
+    let parameters = getParameters();
+    parameters.words.split(',').map((word, windex) => {
+      // Write title
+      div.innerHTML += `<h3>${word}</h3>`;
+      results[windex].map((res, rindex) => {
+        // write the index of each result
+        div.innerHTML += `${rindex+1}<br>`;
+        parameters.fields.map((singleField, findex) => {
+          // now we display properly the results
+          div.innerHTML += singleField + ' : ';
+          let details = results[windex][rindex][findex];
+
+          if(details == null) {
+            div.innerHTML += 'No info';
+          } else if (typeof details === 'object'){
+            div.innerHTML += JSON.stringify(details, null, 4);
+          } else if ((details.includes('http:') || details.includes('https:'))) {
+            // We don't want use url breaking our layout, and a link is friendlier
+            div.innerHTML += `<a href=${details}>Link</a>`;
+          } else {
+            div.innerHTML += JSON.stringify(details, null, 4);
+          }
+          div.innerHTML += '<br>';
+      });
+      });
+      
+    });
   }
 })();
+
+
+
+
